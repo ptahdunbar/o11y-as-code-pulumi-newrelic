bab617ecd990f277436a95f994617bc65e42b81e
(afro)TekGod <pdunbar@newrelic.com>
2023-02-24 16:46:21 -0500

    Delete o11y directory

--- o11y/resources/Tags.ts
+++
@@ -1,9 +0,0 @@
-import * as newrelic from '@pulumi/newrelic';
-
-export default async function (app: any, tags: any) {
-  // Create tags for the application
-  return new newrelic.EntityTags(`tags-${app.name}`, {
-    guid: app.guid,
-    tags,
-  })
-}
